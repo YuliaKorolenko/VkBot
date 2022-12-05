ddcd1c8c9d46277d3db2f1f8ff58414439b12175
Юлия Koроленко <KorolenkoYulia>
2022-12-05 17:24:02 +0300

    tr

--- index.php
+++ index.php
@@ -15,13 +15,13 @@ switch ($data->type) {
         break;
     case 'message_new':
         $user_id = $data->object->message->from_id;
-        $user_info = json_decode(file_get_contents("https://api.vk.com/method/users.get?user_ids={$user_id}&access_token={$token}&v=5.131"));
+        $user_info = json_decode(file_get_contents("https://api.vk.com/method/users.get?user_ids={$user_id}&access_token={$token}&v=5.103"));
         $user_name = $user_info->response[0]->first_name;
         $request_params = array(
             'message' => "Hello, {$user_name}!",
             'peer_id' => $user_id,
             'access_token' => $token,
-            'v' => '5.131',
+            'v' => '5.103',
             'random_id' => '0'
         );
         $get_params = http_build_query($request_params);
