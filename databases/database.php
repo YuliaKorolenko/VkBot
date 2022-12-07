b190037b5470cc749d0afd3ff6111ed444cc5daa
Юлия Koроленко <KorolenkoYulia>
2022-12-07 13:34:10 +0300

    b

--- databases/database.php
+++ databases/database.php
@@ -4,7 +4,7 @@ require_once 'global.php';
 class Database {
     private $host = "109.120.183.78";
     private $database_name = "event";
-    private $username = "ubuntu";
+    private $username = "root";
     private $password = "root";
     public $conn;
 
