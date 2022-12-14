5f21267ef9af538bb88190c8b16567356a7c92b6
Юлия Koроленко <KorolenkoYulia>
2022-12-14 22:50:39 +0300

    add keyboards

--- index.php
+++ index.php
@@ -49,10 +49,10 @@ switch ($data->type) {
         $request_params = array(
             'message' => "Ваша группа, {$user_name}, вот такая! {$data->object->message->text}",
             'peer_id' => $user_id,
-            'keyboard' => MAIN_KEYBOARD,
             'access_token' => $token,
             'v' => '5.103',
-            'random_id' => '0'
+            'random_id' => '0',
+            'keyboard'    => json_encode(MAIN_KEYBOARD, JSON_UNESCAPED_UNICODE)
         );
         $get_params = http_build_query($request_params);
         file_get_contents('https://api.vk.com/method/messages.send?' . $get_params);
