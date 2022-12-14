49d6d4f578a8d8cf4936d53142102d967ccc1a52
Юлия Koроленко <KorolenkoYulia>
2022-12-15 00:55:36 +0300

    add keyboards

--- keyboards.php
+++ keyboards.php
@@ -1,9 +1,10 @@
 <?php
+
 const MAIN_KEYBOARD = [
     "one_time" => false,
     "buttons" => [[
         ["action" => [
             "type" => "text",
-            "payload" => '{"command": oj"}',
-            "label" => "new"],
+            "payload" => '{"command": \'oj\'"}',
+            "label" => "newnew"],
             "color" => "secondary"]]]];
