2aad87dae0c1dd10b4526a8cc3d1fd60c5396e99
Юлия Koроленко <KorolenkoYulia>
2022-12-15 16:29:36 +0300

    add keyboard

--- index.php
+++ index.php
@@ -47,11 +47,22 @@ switch ($data->type) {
 //
 //        log_msg("sucess5");
 
-        start_bot($data, $token);
+        $user_id = $data['peer_id'];
+
+        $request_params = array(
+            'message' => "Ваша группа, {$user_id}, вот такая! {$data->object->message->text}",
+            'peer_id' => $user_id,
+            'access_token' => $token,
+            'random_id' => '0',
+            'keyboard' => json_encode(MAIN_KEYBOARD, JSON_UNESCAPED_UNICODE),
+            'v' => '5.131',
+        );
+
+        start_bot($request_params);
         break;
     case 'message_event':
         $request_params = array(
-            'message' => "Message event, {$data->object->payload} {$data->object->message->payload->label}",
+            'message' => "Message event, {$data->object->message->payload} {$data->object->message->payload->label}",
             'peer_id' => $data->object->message->from_id,
             'access_token' => $token,
             'v' => '5.131',
@@ -64,18 +75,8 @@ switch ($data->type) {
 }
 
 
-function start_bot($data, $token)
+function start_bot($request_params)
 {
-    $user_id = $data['peer_id'];
-
-    $request_params = array(
-        'message' => "Ваша группа, {$user_id}, вот такая! {$data->object->message->text}",
-        'peer_id' => $user_id,
-        'access_token' => $token,
-        'random_id' => '0',
-        'keyboard' => json_encode(MAIN_KEYBOARD, JSON_UNESCAPED_UNICODE),
-        'v' => '5.131',
-    );
 
     $get_params = http_build_query($request_params);
     file_get_contents('https://api.vk.com/method/messages.send?' . $get_params);
