85388c171ceb9754dcdecccebe08006723ae0b46
Юлия Koроленко <KorolenkoYulia>
2022-12-16 13:35:38 +0300

    StartState

--- States/CollectStates.php
+++ States/CollectStates.php
@@ -16,7 +16,9 @@ class CollectStates
 
     public function getState(string $name): ?State
     {
+        log_msg("getStateStart");
         foreach ($this->states as $state) {
+            log_msg($state->getName());
             if (strcmp("Выбрать группу", $state->getName()))
             return $state;
         }
