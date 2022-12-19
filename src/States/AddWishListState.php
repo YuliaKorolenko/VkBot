5906b7bbd77c124161090dff41a75f353baf3e07
Юлия Koроленко <KorolenkoYulia>
2022-12-19 22:12:23 +0300

    AddWishList

--- src/States/AddWishListState.php
+++ src/States/AddWishListState.php
@@ -2,12 +2,15 @@
 
 namespace App\States;
 
-use App\Classes\Group;
 use App\Classes\Users;
 use App\Databases\Database;
 
-class AddGroupState implements State
+class AddWishListState implements State
 {
+
+    public function __construct()
+    {
+    }
 
     public function changeState($data)
     {
@@ -28,31 +31,13 @@ class AddGroupState implements State
 
         $this->_do($data);
     }
-    public function __construct()
-    {
-    }
 
     public function _do($data)
     {
         $user_id = $data->object->message->from_id;
-        $database = new Database();
-        $db = $database->getConnection();
-
-        $item = new Group($db);
-        $item->id = $data->object->message->text;
-        $item->name = "empty";
-        $item->reg_open = 1;
-        $item->price = 0;
-        $item->created = date('Y-m-d H:i:s');
-
-        if ($item->create()) {
-            log_msg("Group created successfully.");
-        } else {
-            log_msg("Group could not be created.");
-        }
 
         $request_params = array(
-            'message' =>  STRING_ADD,
+            'message' => STRING_WISH_LIST,
             'peer_id' => $user_id,
             'access_token' => BOT_TOKEN,
             'random_id' => '0',
@@ -64,20 +49,18 @@ class AddGroupState implements State
         file_get_contents('https://api.vk.com/method/messages.send?' . $get_params);
     }
 
-    public function getName(): string
-    {
-        return "Добавить группу";
-    }
-
-    public function getPreviousName(): string
-    {
-        return "Создать группу";
-    }
-
     public function _error($data)
     {
         // TODO: Implement _error() method.
     }
 
+    public function getName(): string
+    {
+        return "Добавить пользователя";
+    }
 
+    public function getPreviousName(): string
+    {
+        return "Добавить группу";
+    }
 }
