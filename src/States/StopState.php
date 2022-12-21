ea7506556a7393c01c7f979cef8e09fed0e9381d
Юлия Koроленко <KorolenkoYulia>
2022-12-21 03:19:33 +0300

    Base

--- src/States/StopState.php
+++ src/States/StopState.php
@@ -49,7 +49,7 @@ class StopState extends BaseState implements State
 
             $request_params = array(
                 'message' => LINK . $ward_user_id . STOP . $ward["wish_list"],
-                'peer_id' => 229231226,
+                'peer_id' => $santa_user_id,
                 'access_token' => BOT_TOKEN,
                 'random_id' => '0',
                 'keyboard' => json_encode(CREATE_KEYBOARD, JSON_UNESCAPED_UNICODE),
